7820d41e4d4310f4ae0824e8f958421631e52c93
KonstantinSh <Kverde@yandex.ru>
2022-10-04 22:26:25 +0500

    + Paste shortcuts to clipboard

--- source/u_main.pas
+++ source/u_main.pas
@@ -6,13 +6,17 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
-  Windows;
+  Menus, Windows, fgl;
 
 type
+
+  TStringMap = specialize TFPGMap<string, string>;
+
 
   { TfMain }
 
   TfMain = class(TForm)
+    pmPaste: TPopupMenu;
     TrayIcon: TTrayIcon;
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
@@ -20,10 +24,23 @@ type
     procedure TrayIconDblClick(Sender: TObject);
   private
     FHotKeyReplaceCRLFID: Integer;
+    FHotKeyPasteMenuId: Integer;
+
+    // Shortcuts
+
+    FShortcuts: TStringMap;
+
+    procedure LoadShortcuts();
+    procedure ConstructPasteMenu(const AShortcutMap: TStringMap);
+    procedure HandlePasteMenu(Sender: TObject);
+
+
+    // Events
 
     procedure WMHotKey(var Msg:TMessage); message WM_HOTKEY;
 
     procedure ReplaceCRLFInClipboard();
+    procedure ShowPasteMenu();
 
   public
 
@@ -38,24 +55,36 @@ implementation
 {$R *.lfm}
 
 uses
-  Clipbrd,
+  Clipbrd, IniFiles,
   u_utils;
 
 { TfMain }
 
 procedure TfMain.FormCreate(Sender: TObject);
-const
-  VK_A = $41;
 begin
+  FShortcuts := TStringMap.Create;
+
+  LoadShortcuts();
+  ConstructPasteMenu(FShortcuts);
+
+  // Hotkey description
+  // https://www.askingbox.com/tutorial/delphi-system-wide-hotkey
+
   // HotKey CTRL + W
   FHotKeyReplaceCRLFID := GlobalAddAtom('FHotKeyReplaceCRLFID');
   RegisterHotKey(Handle, FHotKeyReplaceCRLFID, MOD_CONTROL, VK_W);
+
+  // HotKey CTRL + E
+  FHotKeyPasteMenuId := GlobalAddAtom('FHotKeyPasteMenuId');
+  RegisterHotKey(Handle, FHotKeyPasteMenuId, MOD_CONTROL, VK_E);
 end;
 
 procedure TfMain.FormDestroy(Sender: TObject);
 begin
   UnRegisterHotKey(Handle, FHotKeyReplaceCRLFID);
   GlobalDeleteAtom(FHotKeyReplaceCRLFID);
+
+  FreeAndNil(FShortcuts);
 end;
 
 procedure TfMain.FormWindowStateChange(Sender: TObject);
@@ -75,15 +104,71 @@ begin
   TrayIcon.Visible := false;
 end;
 
+procedure TfMain.LoadShortcuts;
+const
+  IniFileName = 'pgtool_config.ini';
+  SettingsSectionName = 'shortcuts';
+var
+  IniFile: TIniFile;
+  Settings: TStrings;
+  SettingName: string;
+begin
+  IniFile := nil;
+  Settings := nil;
+
+  IniFile := TIniFile.Create(IniFileName);
+  Settings := TStringList.Create;
+
+  try
+    IniFile.ReadSection(SettingsSectionName, Settings);
+    for SettingName in Settings do
+      FShortcuts.Add(SettingName, IniFile.ReadString(SettingsSectionName, SettingName, ''));
+  finally
+    FreeAndNil(IniFile);
+    FreeAndNil(Settings);
+  end;
+end;
+
+procedure TfMain.ConstructPasteMenu(const AShortcutMap: TStringMap);
+var
+  MenuItem: TMenuItem;
+  i: Integer;
+begin
+  for i := 0 to AShortcutMap.Count - 1 do
+  begin
+    MenuItem := TMenuItem.Create(Self);
+    MenuItem.Caption := AShortcutMap.Keys[i];
+    MenuItem.OnClick := @HandlePasteMenu;
+    pmPaste.Items.Add(MenuItem);
+  end;
+end;
+
+procedure TfMain.HandlePasteMenu(Sender: TObject);
+var
+  Shortcut: string;
+begin
+  Shortcut := FShortcuts[TMenuItem(Sender).Caption];
+  Clipboard.AsText := Shortcut;
+end;
+
+
 procedure TfMain.WMHotKey(var Msg: TMessage);
 begin
-  if Msg.wParam = FHotKeyReplaceCRLFID  then
+  if Msg.wParam = FHotKeyReplaceCRLFID then
     ReplaceCRLFInClipboard;
+
+  if Msg.wParam = FHotKeyPasteMenuId then
+    ShowPasteMenu;
 end;
 
 procedure TfMain.ReplaceCRLFInClipboard;
 begin
   Clipboard.AsText := RemoveCRLF(Clipboard.AsText);
+end;
+
+procedure TfMain.ShowPasteMenu;
+begin
+  pmPaste.PopUp(Mouse.CursorPos.X, Mouse.CursorPos.Y);
 end;
 
 end.
